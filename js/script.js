75f23b60d9e68d3c0b739b368f1d9b885ed456e3
SONNY KEOBOUNPHANH <SKEOBOUNPHANH@GMAIL.COM>
2017-05-15 17:00:39 -0400

    W.I.P

--- js/script.js
+++ js/script.js
@@ -1,14 +1,46 @@
-// // This array holds the words we are going to choose from.
-// // Feel free to add new words!
-// var words = ['cat', 'tree', 'swing', 'around', 'scientist'];
-//
-// // This function will pick our word
-// function chooseWord () {
-//     // Write code here
-//     var chosenWord = Math.floor(Math.random() * words.length);
-//     console.log(chosenWord);
-// }
 
 $(document).ready(function() {
-  alert("hello")
+  var words = [];
+  var values = [];
+
+  $('#playerOne .submit').click(function() {
+    var wordInput = $('#playerOne .input').val();
+    words.push(wordInput)
+    console.log(words);
+    var splitWord = words[0].split("");
+    console.log(splitWord);
+
+    // var nospace = splitWord.filter(function(entry) {
+    //   return entry.trim() != '';
+    // });
+
+    for (var i = 0; i < splitWord.length; i++) {
+      if (splitWord[i] === " ") {
+        splitWord[i] = "\xa0\xa0 "
+      }else{
+      splitWord[i] = "_ "
+      }
+    }
+    $('.wordDisplay').html(splitWord);
+  })
+
+
+  $('#playerTwo .submit').click(function() {
+    var guessInput = $('#playerTwo .input').val();
+    values.push(guessInput)
+    console.log(values);
+  })
+  //generate words randomly from array of pre-exisiting choices
+  // var randomWord = words[Math.floor(Math.random() * words.length)];
+  // console.log(randomWord);
+
+  //split each character from the randomly chosen word in an array
+  // var splitWord = words[0].split("");
+  // console.log(splitWord);
+  //
+  // //display each characters as _ on body
+  // for (var i = 0; i < splitWord.length; i++) {
+  //   splitWord[i] = "_ "
+  // }
+  // $('.display').html(splitWord);
 })
