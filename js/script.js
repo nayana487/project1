7b732d6986e2de2f6af936b0d0be7ed29568f3a7
SONNY KEOBOUNPHANH <SKEOBOUNPHANH@GMAIL.COM>
2017-05-15 11:48:43 -0400

    project set up complete

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// // This array holds the words we are going to choose from.
+// // Feel free to add new words!
+// var words = ['cat', 'tree', 'swing', 'around', 'scientist'];
+//
+// // This function will pick our word
+// function chooseWord () {
+//     // Write code here
+//     var chosenWord = Math.floor(Math.random() * words.length);
+//     console.log(chosenWord);
+// }
+
+$(document).ready(function() {
+  alert("hello")
+})
